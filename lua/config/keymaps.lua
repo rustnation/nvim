8af580cf8f166a8de79dd19be49b7f39ae8b89ec
William Munoz <william@rustdeveloper.rs>
2025-10-03 23:01:41 -0500

    implement move between buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- -- navigate between buffers
+-- use `vim.keymap.set` instead
+local map = vim.keymap.set
+map("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Open next buffer" })
+map("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Open previous buffer" })
